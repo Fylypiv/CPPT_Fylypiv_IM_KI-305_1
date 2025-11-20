0b9a0a5f103b23846b2064e0590c292d4ea003d0
Fylypiv <fyllypivigor@gmail.com>
2025-11-20 17:33:13 +0200

    Add files via upload

---
+++ Lab_4/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"KI305.Fylypiv.Lab4"}];updateSearchResults();
---
+++ Lab_4/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI305.Fylypiv.Lab4","l":"ExpressionCalculator"},{"p":"KI305.Fylypiv.Lab4","l":"Lab4Main"}];updateSearchResults();
